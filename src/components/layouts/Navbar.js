a392fd457d08e50081d17cced39bfd8a591a087d
vijayrjdev@gmail.com <vijayrajck@outlook.com>
2022-03-20 19:43:36 +0530

    updates

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,33 +1,16 @@
 import {
   Box,
   Flex,
-  Link,
   Button,
-  useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-const NavLink = ({ children }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: useColorModeValue("gray.200", "gray.700"),
-    }}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
-
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
